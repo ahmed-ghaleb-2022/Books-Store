19449e2a71b7b97c6197acdffd9a44a7a00a37ee
ahmed-ghaleb-2022 <ahmedtareq19932022@gmail.com>
2024-12-28 14:30:43 +0100

    remove some unused imports from app file

--- frontend/src/components/Reviews/Reviews.tsx
+++ frontend/src/components/Reviews/Reviews.tsx
@@ -3,8 +3,8 @@ import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
 
 import './Reviews.css';
 
